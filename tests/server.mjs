d8167c7a0e68742e09d212bb6d84bccab58b9831
Camillo Bruni <cbruni@chromium.org>
2025-01-28 14:11:41 +0100

    simpler and more robust startup

--- tests/server.mjs
+++ tests/server.mjs
@@ -1,8 +1,13 @@
-// Simple local server
+// Simple server for local testing.
+
 import * as path from "path";
 import commandLineArgs from "command-line-args";
 import esMain from "es-main";
-import LocalWebServer from "local-web-server";
+import LocalWebServer from "lws";
+import "lws-cors";
+import "lws-index";
+import "lws-log";
+import "lws-static";
 
 const ROOT_DIR = path.join(process.cwd(), "./");
 
@@ -14,20 +19,35 @@ export default async function serve(port) {
         directory: ROOT_DIR,
         corsOpenerPolicy: "same-origin",
         corsEmbedderPolicy: "require-corp",
+        logFormat: "dev",
+        stack: ["lws-log", "lws-cors", "lws-static", "lws-index"],
     });
-    console.log(`Server started on http://localhost:${port}`);
+    await verifyStartup(ws, port);
+
     process.on("exit", () => ws.server.close());
+
     return {
         close() {
             ws.server.close();
-        }
+        },
     };
 }
 
+async function verifyStartup(ws, port) {
+    await new Promise((resolve, reject) => {
+        ws.server.on("listening", () => {
+            console.log(`Server started on http://localhost:${port}`);
+            resolve();
+        });
+        ws.server.on("error", (e) => {
+            console.error("Error while starting the server", e);
+            reject(e);
+        });
+    });
+}
+
 function main() {
-    const optionDefinitions = [
-        { name: "port", type: Number, defaultValue: 8010, description: "Set the test-server port, The default value is 8010." },
-    ];
+    const optionDefinitions = [{ name: "port", type: Number, defaultValue: 8010, description: "Set the test-server port, The default value is 8010." }];
     const options = commandLineArgs(optionDefinitions);
     serve(options.port);
 }
