495e388740b2ae47810405c1c2c3c73ed2ac11ab
Daniel Lehmann <dlehmann@chromium.org>
2025-01-29 14:44:50 +0100

    Add zlib-wasm workload
    As a replacement for the asm.js zlib workload in Octane that we removed.
    Based on public domain zpipe.c example from https://www.zlib.net/zlib_how.html.
    Compresses about 60KB of Wasm binary, and decompresses again.
    Built with Emscripten, using zlib Emscripten port.
    A single iteration takes around 50ms, the total line item around 2s.

---
+++ wasm/zlib/src/main.c
@@ -0,0 +1,49 @@
+// Copyright 2025 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <stdio.h>
+#include "zlib.h"
+
+// To be compiled together with `zpipe.c` from zlib. Forward declarations:
+int def(FILE *source, FILE *dest, int level);
+int inf(FILE *source, FILE *dest);
+void zerr(int ret);
+
+// Exported to JavaScript:
+int compressFile(const char* infilename, const char* outfilename)
+{
+    FILE* infile = fopen(infilename, "rb");
+    if (infile == NULL) {
+        fputs("error opening input file", stderr);
+        return 1;
+    }
+    FILE* outfile = fopen(outfilename, "wb");
+    if (outfile == NULL) {
+        fputs("error opening output file", stderr);
+        return 1;
+    }
+
+    int ret = def(infile, outfile, Z_BEST_COMPRESSION);
+    if (ret != Z_OK)
+        zerr(ret);
+    return ret;
+}
+int decompressFile(const char* infilename, const char* outfilename)
+{
+    FILE* infile = fopen(infilename, "rb");
+    if (infile == NULL) {
+        fputs("error opening input file", stderr);
+        return 1;
+    }
+    FILE* outfile = fopen(outfilename, "wb");
+    if (outfile == NULL) {
+        fputs("error opening output file", stderr);
+        return 1;
+    }
+
+    int ret = inf(infile, outfile);
+    if (ret != Z_OK)
+        zerr(ret);
+    return ret;
+}
