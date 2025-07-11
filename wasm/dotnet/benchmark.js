79df4d0dfe858230556c1272a818c382c77646c9
Marek Fišera <mara@neptuo.com>
2025-07-11 15:00:04 +0200

    Use getBinary

--- wasm/dotnet/benchmark.js
+++ wasm/dotnet/benchmark.js
@@ -1,138 +1,134 @@
 class Benchmark {
     async init() {
-        async function fetchBin(name) {
-            return new Uint8Array(await (await fetch(name)).arrayBuffer());
-        }
+        const config = {
+            mainAssemblyName: "dotnet.dll",
+            globalizationMode: "custom",
+            assets: [
+                {
+                    name: "dotnet.runtime.js",
+                    resolvedUrl: dotnetRuntimeUrl,
+                    moduleExports: await dynamicImport(dotnetRuntimeUrl),
+                    behavior: "js-module-runtime"
+                },
+                {
+                    name: "dotnet.native.js",
+                    resolvedUrl: dotnetNativeUrl,
+                    moduleExports: await dynamicImport(dotnetNativeUrl),
+                    behavior: "js-module-native"
+                },
+                {
+                    name: "dotnet.native.wasm",
+                    resolvedUrl: wasmBinaryUrl,
+                    buffer: await getBinary(wasmBinaryUrl),
+                    behavior: "dotnetwasm"
+                },
+                {
+                    name: "icudt_CJK.dat",
+                    resolvedUrl: icuCustomUrl,
+                    buffer: await getBinary(icuCustomUrl),
+                    behavior: "icu"
+                },
+                {
+                    name: "System.Collections.Concurrent.wasm",
+                    resolvedUrl: dllCollectionsConcurrentUrl,
+                    buffer: await getBinary(dllCollectionsConcurrentUrl),
+                    behavior: "assembly"
+                },
+                {
+                    name: "System.Collections.wasm",
+                    resolvedUrl: dllCollectionsUrl,
+                    buffer: await getBinary(dllCollectionsUrl),
+                    behavior: "assembly"
+                },
+                {
+                    name: "System.ComponentModel.Primitives.wasm",
+                    resolvedUrl: dllComponentModelPrimitivesUrl,
+                    buffer: await getBinary(dllComponentModelPrimitivesUrl),
+                    behavior: "assembly"
+                },
+                {
+                    name: "System.ComponentModel.TypeConverter.wasm",
+                    resolvedUrl: dllComponentModelTypeConverterUrl,
+                    buffer: await getBinary(dllComponentModelTypeConverterUrl),
+                    behavior: "assembly"
+                },
+                {
+                    name: "System.Drawing.Primitives.wasm",
+                    resolvedUrl: dllDrawingPrimitivesUrl,
+                    buffer: await getBinary(dllDrawingPrimitivesUrl),
+                    behavior: "assembly"
+                },
+                {
+                    name: "System.Drawing.wasm",
+                    resolvedUrl: dllDrawingUrl,
+                    buffer: await getBinary(dllDrawingUrl),
+                    behavior: "assembly"
+                },
+                {
+                    name: "System.IO.Pipelines.wasm",
+                    resolvedUrl: dllIOPipelinesUrl,
+                    buffer: await getBinary(dllIOPipelinesUrl),
+                    behavior: "assembly"
+                },
+                {
+                    name: "System.Linq.wasm",
+                    resolvedUrl: dllLinqUrl,
+                    buffer: await getBinary(dllLinqUrl),
+                    behavior: "assembly"
+                },
+                {
+                    name: "System.Memory.wasm",
+                    resolvedUrl: dllMemoryUrl,
+                    buffer: await getBinary(dllMemoryUrl),
+                    behavior: "assembly"
+                },
+                {
+                    name: "System.ObjectModel.wasm",
+                    resolvedUrl: dllObjectModelUrl,
+                    buffer: await getBinary(dllObjectModelUrl),
+                    behavior: "assembly"
+                },
+                {
+                    name: "System.Private.CoreLib.wasm",
+                    resolvedUrl: dllPrivateCorelibUrl,
+                    buffer: await getBinary(dllPrivateCorelibUrl),
+                    behavior: "assembly"
+                },
+                {
+                    name: "System.Runtime.InteropServices.JavaScript.wasm",
+                    resolvedUrl: dllRuntimeInteropServicesJavaScriptUrl,
+                    buffer: await getBinary(dllRuntimeInteropServicesJavaScriptUrl),
+                    behavior: "assembly"
+                },
+                {
+                    name: "System.Text.Encodings.Web.wasm",
+                    resolvedUrl: dllTextEncodingsWebUrl,
+                    buffer: await getBinary(dllTextEncodingsWebUrl),
+                    behavior: "assembly"
+                },
+                {
+                    name: "System.Text.Json.wasm",
+                    resolvedUrl: dllTextJsonUrl,
+                    buffer: await getBinary(dllTextJsonUrl),
+                    behavior: "assembly"
+                },
+                {
+                    name: "dotnet.wasm",
+                    resolvedUrl: dllAppUrl,
+                    buffer: await getBinary(dllAppUrl),
+                    behavior: "assembly"
+                }
+            ]
+        };
 
-        if (isInBrowser) {
-            this.dotnetUrl = dotnetUrl;
-            this.config = {
-                mainAssemblyName: "dotnet.dll",
-                globalizationMode: "custom",
-                assets: [
-                    {
-                        name: "dotnet.runtime.js",
-                        resolvedUrl: dotnetRuntimeUrl,
-                        moduleExports: await import(dotnetRuntimeUrl),
-                        behavior: "js-module-runtime"
-                    },
-                    {
-                        name: "dotnet.native.js",
-                        resolvedUrl: dotnetNativeUrl,
-                        moduleExports: await import(dotnetNativeUrl),
-                        behavior: "js-module-native"
-                    },
-                    {
-                        name: "dotnet.native.wasm",
-                        resolvedUrl: wasmBinaryUrl,
-                        buffer: await fetchBin(wasmBinaryUrl),
-                        behavior: "dotnetwasm"
-                    },
-                    {
-                        name: "icudt_CJK.dat",
-                        resolvedUrl: icuCustomUrl,
-                        buffer: await fetchBin(icuCustomUrl),
-                        behavior: "icu"
-                    },
-                    {
-                        name: "System.Collections.Concurrent.wasm",
-                        resolvedUrl: dllCollectionsConcurrentUrl,
-                        buffer: await fetchBin(dllCollectionsConcurrentUrl),
-                        behavior: "assembly"
-                    },
-                    {
-                        name: "System.Collections.wasm",
-                        resolvedUrl: dllCollectionsUrl,
-                        buffer: await fetchBin(dllCollectionsUrl),
-                        behavior: "assembly"
-                    },
-                    {
-                        name: "System.ComponentModel.Primitives.wasm",
-                        resolvedUrl: dllComponentModelPrimitivesUrl,
-                        buffer: await fetchBin(dllComponentModelPrimitivesUrl),
-                        behavior: "assembly"
-                    },
-                    {
-                        name: "System.ComponentModel.TypeConverter.wasm",
-                        resolvedUrl: dllComponentModelTypeConverterUrl,
-                        buffer: await fetchBin(dllComponentModelTypeConverterUrl),
-                        behavior: "assembly"
-                    },
-                    {
-                        name: "System.Drawing.Primitives.wasm",
-                        resolvedUrl: dllDrawingPrimitivesUrl,
-                        buffer: await fetchBin(dllDrawingPrimitivesUrl),
-                        behavior: "assembly"
-                    },
-                    {
-                        name: "System.Drawing.wasm",
-                        resolvedUrl: dllDrawingUrl,
-                        buffer: await fetchBin(dllDrawingUrl),
-                        behavior: "assembly"
-                    },
-                    {
-                        name: "System.IO.Pipelines.wasm",
-                        resolvedUrl: dllIOPipelinesUrl,
-                        buffer: await fetchBin(dllIOPipelinesUrl),
-                        behavior: "assembly"
-                    },
-                    {
-                        name: "System.Linq.wasm",
-                        resolvedUrl: dllLinqUrl,
-                        buffer: await fetchBin(dllLinqUrl),
-                        behavior: "assembly"
-                    },
-                    {
-                        name: "System.Memory.wasm",
-                        resolvedUrl: dllMemoryUrl,
-                        buffer: await fetchBin(dllMemoryUrl),
-                        behavior: "assembly"
-                    },
-                    {
-                        name: "System.ObjectModel.wasm",
-                        resolvedUrl: dllObjectModelUrl,
-                        buffer: await fetchBin(dllObjectModelUrl),
-                        behavior: "assembly"
-                    },
-                    {
-                        name: "System.Private.CoreLib.wasm",
-                        resolvedUrl: dllPrivateCorelibUrl,
-                        buffer: await fetchBin(dllPrivateCorelibUrl),
-                        behavior: "assembly"
-                    },
-                    {
-                        name: "System.Runtime.InteropServices.JavaScript.wasm",
-                        resolvedUrl: dllRuntimeInteropServicesJavaScriptUrl,
-                        buffer: await fetchBin(dllRuntimeInteropServicesJavaScriptUrl),
-                        behavior: "assembly"
-                    },
-                    {
-                        name: "System.Text.Encodings.Web.wasm",
-                        resolvedUrl: dllTextEncodingsWebUrl,
-                        buffer: await fetchBin(dllTextEncodingsWebUrl),
-                        behavior: "assembly"
-                    },
-                    {
-                        name: "System.Text.Json.wasm",
-                        resolvedUrl: dllTextJsonUrl,
-                        buffer: await fetchBin(dllTextJsonUrl),
-                        behavior: "assembly"
-                    },
-                    {
-                        name: "dotnet.wasm",
-                        resolvedUrl: dllAppUrl,
-                        buffer: await fetchBin(dllAppUrl),
-                        behavior: "assembly"
-                    }
-                ]
-            };
-        } else {
-            this.config = {};
-            this.dotnetUrl = `./wasm/dotnet/build-${dotnetFlavor}/wwwroot/_framework/dotnet.js`;
+        for (let index = 0; index < config.assets.length; index++) {
+            const element = config.assets[index];
+            console.log(`Asset ${index}: ${element.name} ${element.buffer?.length}`);
         }
+        debugger;
 
-        this.dotnet = (await import(this.dotnetUrl)).dotnet;
-        this.api = await this.dotnet.withModuleConfig({ locateFile: e => e }).withConfig(this.config).create();
+        this.dotnet = (await dynamicImport(dotnetUrl)).dotnet;
+        this.api = await this.dotnet.withModuleConfig({ locateFile: e => e }).withConfig(config).create();
         this.exports = await this.api.getAssemblyExports("dotnet.dll");
         
         const hardwareConcurrency = globalThis.navigator?.hardwareConcurrency ?? 1;
