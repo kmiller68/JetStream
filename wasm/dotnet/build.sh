ded3bbd90473236b50672dedfb7cc198e1d8fc02
Marek Fišera <mara@neptuo.com>
2025-07-15 13:47:23 +0200

    .NET SDK info

--- wasm/dotnet/build.sh
+++ wasm/dotnet/build.sh
@@ -1,4 +1,8 @@
-# Download .NET SDK 9.0.300
-# Install workload wasm-tools
+#! /bin/sh
+
+# Expects to have .NET SDK 9.0.3xx,
+# downloadable from using https://aka.ms/dotnet/9.0.3xx/daily/dotnet-sdk-win-x64.zip or https://aka.ms/dotnet/9.0.3xx/daily/dotnet-sdk-linux-x64.tar.gz
+
+dotnet workload install wasm-tools
 dotnet publish -o ./build-interp ./src/dotnet/dotnet.csproj
 dotnet publish -o ./build-aot ./src/dotnet/dotnet.csproj -p:RunAOTCompilation=true
