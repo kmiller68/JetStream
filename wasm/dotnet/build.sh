295ae1a5c0faa05c8cfebc0507b2980b973186b1
Marek Fišera <mara@neptuo.com>
2025-07-16 15:03:43 +0200

    Prepend import.meta.url and copy symbols from obj

--- wasm/dotnet/build.sh
+++ wasm/dotnet/build.sh
@@ -5,4 +5,9 @@
 
 dotnet workload install wasm-tools
 dotnet publish -o ./build-interp ./src/dotnet/dotnet.csproj
+printf '%s\n' 'import.meta.url ??= "";' | cat - ./build-interp/wwwroot/_framework/dotnet.js > temp.js && mv temp.js ./build-interp/wwwroot/_framework/dotnet.js
+cp ./src/dotnet/obj/Release/net9.0/wasm/for-publish/dotnet.native.js.symbols ./build-interp/wwwroot/_framework/
+
 dotnet publish -o ./build-aot ./src/dotnet/dotnet.csproj -p:RunAOTCompilation=true
+printf '%s\n' 'import.meta.url ??= "";' | cat - ./build-aot/wwwroot/_framework/dotnet.js > temp.js && mv temp.js ./build-aot/wwwroot/_framework/dotnet.js
+cp ./src/dotnet/obj/Release/net9.0/wasm/for-publish/dotnet.native.js.symbols ./build-aot/wwwroot/_framework/
