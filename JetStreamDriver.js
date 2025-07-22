03a06211be788e2362f0f407e287835c9ed02264
Ryan Hunt <rhunt@eqrion.net>
2025-07-22 11:03:55 -0500

    Add Dart-flute-todomvc in addition to complex
    Update to the latest wasm_gc_benchmarks commit, and pull in the todomvc
    benchmark too.
    
    I had to update the benchmark runner polyfill. I just copy pasted things
    from tools/run_wasm.js until it worked for me. This part likely needs
    some filtering and clean up.

--- JetStreamDriver.js
+++ JetStreamDriver.js
@@ -740,7 +740,7 @@ class Benchmark {
                 __benchmark.runIteration();
                 let end = performance.now();
 
-                performance.measure(iterationMarkLabel, iterationMarkLabel);
+                // performance.measure(iterationMarkLabel, iterationMarkLabel);
 
                 ${this.postIterationCode}
 
@@ -1233,7 +1233,7 @@ class AsyncBenchmark extends DefaultBenchmark {
                 await __benchmark.runIteration();
                 let end = performance.now();
 
-                performance.measure(iterationMarkLabel, iterationMarkLabel);
+                // performance.measure(iterationMarkLabel, iterationMarkLabel);
 
                 ${this.postIterationCode}
 
@@ -1314,7 +1314,7 @@ class WSLBenchmark extends Benchmark {
                 benchmark.buildStdlib();
                 results.push(performance.now() - start);
 
-                performance.measure(markLabel, markLabel);
+                // performance.measure(markLabel, markLabel);
             }
 
             {
@@ -1325,7 +1325,7 @@ class WSLBenchmark extends Benchmark {
                 benchmark.run();
                 results.push(performance.now() - start);
 
-                performance.measure(markLabel, markLabel);
+                // performance.measure(markLabel, markLabel);
             }
 
             top.currentResolve(results);
@@ -2057,13 +2057,26 @@ let BENCHMARKS = [
         tags: ["Default", "Wasm"],
     }),
     new WasmEMCCBenchmark({
-        name: "Dart-flute-wasm",
+        name: "Dart-flute-complex-wasm",
         files: [
             "./Dart/benchmark.js",
         ],
         preload: {
-            jsModule: "./Dart/build/flute.dart2wasm.mjs",
-            wasmBinary: "./Dart/build/flute.dart2wasm.wasm",
+            jsModule: "./Dart/build/flute.complex.dart2wasm.mjs",
+            wasmBinary: "./Dart/build/flute.complex.dart2wasm.wasm",
+        },
+        iterations: 15,
+        worstCaseCount: 2,
+        tags: ["Wasm"],
+    }),
+    new WasmEMCCBenchmark({
+        name: "Dart-flute-todomvc-wasm",
+        files: [
+            "./Dart/benchmark.js",
+        ],
+        preload: {
+            jsModule: "./Dart/build/flute.todomvc.dart2wasm.mjs",
+            wasmBinary: "./Dart/build/flute.todomvc.dart2wasm.wasm",
         },
         iterations: 15,
         worstCaseCount: 2,
