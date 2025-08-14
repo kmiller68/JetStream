3b1af037cf0691cc0df2657be835bbf965408b1f
Camillo Bruni <camillobruni@users.noreply.github.com>
2025-08-14 18:36:16 +0200

    Only expose a console copy in the realm (#137)
    Avoid accidentally overriding the default console methods in workloads.

--- JetStreamDriver.js
+++ JetStreamDriver.js
@@ -629,7 +629,9 @@ class ShellScripts extends Scripts {
         } else
             globalObject = runString("");
 
-        globalObject.console = console;
+        // Expose console copy in the realm so we don't accidentally modify
+        // the original object.
+        globalObject.console = Object.assign({}, console);
         globalObject.self = globalObject;
         globalObject.top = {
             currentResolve,
