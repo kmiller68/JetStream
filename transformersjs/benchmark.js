18b6918b61ee16bfe193d7aa9eeef6474807b83d
Daniel Lehmann <dlehmann@chromium.org>
2025-08-18 16:28:55 +0200

    WIP adapt to runner changes, new preloading code

--- transformersjs/benchmark.js
+++ transformersjs/benchmark.js
@@ -50,62 +50,64 @@ class Benchmark {
   inputFile;
 
   async init() {
-    // The generated JavaScript code from dart2wasm is an ES module, which we
-    // can only load with a dynamic import (since this file is not a module.)
-    // TODO: Support ES6 modules in the driver instead of this one-off solution.
-    // This probably requires a new `Benchmark` field called `modules` that
-    // is a map from module variable name (which will hold the resulting module
-    // namespace object) to relative module URL, which is resolved in the
-    // `preRunnerCode`, similar to this code here.
-    if (isInBrowser) {
-      // In browsers, relative imports don't work since we are not in a module.
-      // (`import.meta.url` is not defined.)
-      this.transformersJsModule = await import(location.origin + "/transformersjs/build/transformers.js");
-    } else {
-      // In shells, relative imports require different paths, so try with and
-      // without the "./" prefix (e.g., JSC requires it).
-      try {
-        this.transformersJsModule = await import("transformersjs/build/transformers.js");
-      } catch {
-        this.transformersJsModule = await import("./transformersjs/build/transformers.js");
-      }
-    }
+    this.transformersJsModule = await dynamicImport(globalThis.JetStream.transformersJsModule);
 
-    preload = preloadFiles(Module);
+    // // The generated JavaScript code from dart2wasm is an ES module, which we
+    // // can only load with a dynamic import (since this file is not a module.)
+    // // TODO: Support ES6 modules in the driver instead of this one-off solution.
+    // // This probably requires a new `Benchmark` field called `modules` that
+    // // is a map from module variable name (which will hold the resulting module
+    // // namespace object) to relative module URL, which is resolved in the
+    // // `preRunnerCode`, similar to this code here.
+    // if (isInBrowser) {
+    //   // In browsers, relative imports don't work since we are not in a module.
+    //   // (`import.meta.url` is not defined.)
+    //   this.transformersJsModule = await import(location.origin + "/transformersjs/build/transformers.js");
+    // } else {
+    //   // In shells, relative imports require different paths, so try with and
+    //   // without the "./" prefix (e.g., JSC requires it).
+    //   try {
+    //     this.transformersJsModule = await import("transformersjs/build/transformers.js");
+    //   } catch {
+    //     this.transformersJsModule = await import("./transformersjs/build/transformers.js");
+    //   }
+    // }
 
-    if (Module.inputFile) {
-      this.inputFile = Module.inputFile.buffer;
-    }
+    // preload = preloadFiles(Module);
+
+    // if (Module.inputFile) {
+    //   this.inputFile = Module.inputFile.buffer;
+    // }
   }
 
   async runIteration() {
-    if (!this.pipeline) {
-      // TODO: Profile startup only: What is taking so much time here?
-      let { env, pipeline } = this.transformersJsModule;
+    // if (!this.pipeline) {
+    //   // TODO: Profile startup only: What is taking so much time here?
+    //   let { env, pipeline } = this.transformersJsModule;
     
-      env.localModelPath = 'build/models/';
-      env.allowRemoteModels = false;
+    //   env.localModelPath = 'build/models/';
+    //   env.allowRemoteModels = false;
     
-      // Single-threaded only for now, since we cannot spawn workers in shells.
-      // TODO: Implement sufficiently powerful workers in shells (or provide
-      // polyfills).
-      env.backends.onnx.wasm.numThreads = 1;
+    //   // Single-threaded only for now, since we cannot spawn workers in shells.
+    //   // TODO: Implement sufficiently powerful workers in shells (or provide
+    //   // polyfills).
+    //   env.backends.onnx.wasm.numThreads = 1;
 
-      // Either specify path prefix, but this loads the JSEP build by default.
-      // env.backends.onnx.wasm.wasmPaths = 'build/onnxruntime-web/';
-      // So instead, give the ONNX runtime files directly:
-      env.backends.onnx.wasm.wasmPaths = {
-        mjs: './onnxruntime-web/ort-wasm-simd-threaded.mjs',
-        wasm: 'ort-wasm-simd-threaded.wasm',
-      };
-      // Finally, this would be our preferred option: Giving the Wasm binary
-      // directly, such that we don't have to intercept `instantiateStreaming`,
-      // but see above.
-      // env.backends.onnx.wasm.wasmBinary = ...
+    //   // Either specify path prefix, but this loads the JSEP build by default.
+    //   // env.backends.onnx.wasm.wasmPaths = 'build/onnxruntime-web/';
+    //   // So instead, give the ONNX runtime files directly:
+    //   env.backends.onnx.wasm.wasmPaths = {
+    //     mjs: './onnxruntime-web/ort-wasm-simd-threaded.mjs',
+    //     wasm: 'ort-wasm-simd-threaded.wasm',
+    //   };
+    //   // Finally, this would be our preferred option: Giving the Wasm binary
+    //   // directly, such that we don't have to intercept `instantiateStreaming`,
+    //   // but see above.
+    //   // env.backends.onnx.wasm.wasmBinary = ...
 
-      this.pipeline = await initPipeline(pipeline);
-    }
+    //   this.pipeline = await initPipeline(pipeline);
+    // }
     
-    await doTask(this.pipeline, this.inputFile);
+    // await doTask(this.pipeline, this.inputFile);
   }
 }
