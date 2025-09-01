f8e3d7e50ed5c7ac071a9d90d3ee36cb68a8678c
Camillo Bruni <camillobruni@users.noreply.github.com>
2025-09-01 22:38:11 +0200

    Update/rewrite web-tooling-benchmark (#153)
    The orignal benchmark [1] has been stale for a while and uses a more node-oriented build process.
    
    - Copy complete build scripts + sources to jetstream
    - Strip away the nodejs web-tooling-benchmark and standalone html-runner
    - Update each original workload to the latest version
    
    [1] https://github.com/v8/web-tooling-benchmark

--- JetStreamDriver.js
+++ JetStreamDriver.js
@@ -827,7 +827,7 @@ class Benchmark {
         if (this.plan.preload) {
             let preloadCode = "";
             for (let [ variableName, blobURLOrPath ] of this.preloads)
-                preloadCode += `JetStream.preload.${variableName} = "${blobURLOrPath}";\n`;
+                preloadCode += `JetStream.preload[${JSON.stringify(variableName)}] = "${blobURLOrPath}";\n`;
             scripts.add(preloadCode);
         }
 
@@ -2530,7 +2530,7 @@ let BENCHMARKS = [
         iterations: 15,
         worstCaseCount: 2,
         tags: ["Default", "Wasm", "dotnet"],
-    })
+    }),
 ];
 
 
@@ -2565,28 +2565,67 @@ BENCHMARKS.push(new GroupedBenchmark({
 }, SUNSPIDER_BENCHMARKS))
 
 // WTB (Web Tooling Benchmark) tests
-const WTB_TESTS = [
-    "acorn",
-    "babylon",
-    "chai",
-    "coffeescript",
-    "espree",
-    "jshint",
-    "lebab",
-    "prepack",
-    "uglify-js",
-];
-for (const name of WTB_TESTS) {
-    BENCHMARKS.push(new DefaultBenchmark({
+const WTB_TESTS = {
+    "acorn": true,
+    "babel": true,
+    "babel-minify": true,
+    "babylon": true,
+    "chai": true,
+    "espree": true,
+    "esprima-next": true,
+    // Disabled: Converting ES5 code to ES6+ is no longer a realistic scenario.
+    "lebab": false, 
+    "postcss": true,
+    "prettier": true,
+    "source-map": true,
+};
+const WPT_FILES = [
+  "angular-material-20.1.6.css",
+  "backbone-1.6.1.js",
+  "bootstrap-5.3.7.css",
+  "foundation-6.9.0.css",
+  "jquery-3.7.1.js",
+  "lodash.core-4.17.21.js",
+  "lodash-4.17.4.min.js.map",
+  "mootools-core-1.6.0.js",
+  "preact-8.2.5.js",
+  "preact-10.27.1.min.module.js.map",
+  "redux-5.0.1.min.js",
+  "redux-5.0.1.esm.js",
+  "source-map.min-0.5.7.js.map",
+  "source-map/lib/mappings.wasm",
+  "speedometer-es2015-test-2.0.js",
+  "todomvc/react/app.jsx",
+  "todomvc/react/footer.jsx",
+  "todomvc/react/todoItem.jsx",
+  "todomvc/typescript-angular.ts",
+  "underscore-1.13.7.js",
+  "underscore-1.13.7.min.js.map",
+  "vue-3.5.18.runtime.esm-browser.js",
+].reduce((acc, file) => {
+        acc[file] = `./web-tooling-benchmark/third_party/${file}`;
+        return acc
+}, Object.create(null));
+
+
+for (const [name, enabled] of Object.entries(WTB_TESTS)) {
+    const tags =  ["WTB"];
+    if (enabled)
+        tags.push("Default");
+    BENCHMARKS.push(new AsyncBenchmark({
         name: `${name}-wtb`,
         files: [
-            (isInBrowser ? "./web-tooling-benchmark/browser.js" : "./web-tooling-benchmark/cli.js"),
-            `./web-tooling-benchmark/${name}.js`,
+            `./web-tooling-benchmark/dist/${name}.bundle.js`,
+            "./web-tooling-benchmark/benchmark.js",
         ],
-        iterations: 5,
-        worstCaseCount: 1,
+        preload: {
+            BUNDLE: `./web-tooling-benchmark/dist/${name}.bundle.js`,
+            ...WPT_FILES,
+        },
+        iterations: 15,
+        worstCaseCount: 2,
         allowUtf16: true,
-        tags: ["Default", "WTB"],
+        tags: tags,
     }));
 }
 
